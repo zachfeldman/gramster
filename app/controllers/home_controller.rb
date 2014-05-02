80bb89217ed77323caf2d8acc6792aab83f39b27
Zach Feldman <zachfeldman@gmail.com>
2014-05-02 02:22:23 -0400

    All changes in issue #1. Closes issue #1.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
+require 'instagram'
+
 class HomeController < ApplicationController
   def index
+    client = Instagram.client(:access_token => ENV['INSTAGRAM_CLIENT_TOKEN'])
+    @grams = [] 
+    results = Geocoder.search(Rails.env.production? ? request.remote_ip : "72.227.131.103")
+    for media_item in client.media_search(results[0].latitude, results[0].longitude)
+      @grams.push({image: media_item.images.low_resolution.url, tags: media_item.caption ? media_item.caption.text : media_item.tags.join(" #")})
+    end
   end
 
   def search
--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,11 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Instagram.configure do |config|
+  config.client_id = ENV['INSTAGRAM_CLIENT_ID']
+  config.client_secret = ENV['INSTAGRAM_CLIENT_SECRET']
+end
+
 module Gramster
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
